67cbcb4e51a75daf4209b4015080bfa81bbb0ce9
carlpagelsLNU <cp222ep@student.lnu.se>
2015-12-02 18:15:34 +0100

    Fixed position of comments

--- View/CommentView.php
+++ View/CommentView.php
@@ -4,12 +4,13 @@ class CommentView {
 
 
 	public function getCommentForm() {
-		return '     <div id ="PostComment">
+		return '
           	<form action="" method="post">	
           	<label> Namn: <br><input type="text" name ="name"></br></label>
           	<label> Kommentar:<br><textarea cols="30" rows="5" name ="content"></textarea></br></label>
           	<input type ="submit" name = "post" value = "Skicka kommentar">
           </form>';
+       
 	}
 
 	public function getComments() {
